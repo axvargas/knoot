903670f8622793453415bf63f370856e21ee3937
ltvargas <ltvargas@espol.edu.ec>
2020-06-26 22:45:22 -0500

    ventana flotante EDITAR PERFIL

--- src/views/profile/index.js
+++ src/views/profile/index.js
@@ -3,86 +3,31 @@ import useStyles from '../styles';
 import {Box,
         Typography,
         Grid,
-        Avatar} from '@material-ui/core';
+        Avatar,Button} from '@material-ui/core';
 import Fade from '@material-ui/core/Fade';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Chips from "../../components/chips";
+import Input from '@material-ui/core/Input';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-import './style.css';
-import {Button,Modal,ModalHeader,ModalBody,ModalFooter,fromGroup,Input,Label} from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import state from '@material-ui/core';
-import abrirModal from '@material-ui/core';
-class Favorites extends React.Component{
-    state={
-        abierto: false,
-    }
-
-    abrirModal=()=>{
-        this.setState({abierto: !this.state.abierto});
-    }
-    
-    render(){
-
-        const modalStyles={
-            position: "absolute",
-            top: '50%',
-            left:'50%',
-            transform: 'translate(-50%,-50%)'
-        }    
-    return (
-        <>
-        
-            <h1>Favoritos</h1>
-            <div className="principal">
-                <div className="secundario">
-                <Button color="success" onClick={this.abrirModal}>Editar Perfil</Button>
-                </div>
-            </div>
-            
-
-            <Modal isOpen={this.state.abierto} style={modalStyles}>
-            <ModalHeader>
-                Editar Perfil
-            
-            <ModalBody>
-                <fromGroup>
-                    <br/>
-                    <Input type="text"id="nom" value="Nombre"/>
-                </fromGroup>
-                <fromGroup>
-                    <br/>
-                    <Input type="text"id="ape" value="Apellido"/>
-                </fromGroup>
-                <fromGroup>
-                    <br/>
-                    <Input type="text"id="em" value="E-mail"/>
-                    
-                </fromGroup>
-                <fromGroup>
-                    <br/>
-                    <Input type="text"id="ci" value="Ciudad"/>
-                </fromGroup>
-                <fromGroup>
-                    <br/>
-                    <textarea id="des" row="5" cols="30">Descripción</textarea>
-                </fromGroup>
-            </ModalBody>
-            <ModalFooter>
-                <Button color='primary'>Editar</Button>
-                <Button color='secondary'  onClick={this.abrirModal}>Cancelar</Button>
-            </ModalFooter>
-            </ModalHeader>
-            </Modal>
-
-        </>
-    )
-}}
 
 
 const Profile = () => {
     const classes = useStyles();
+      const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
     return (
         <main className={classes.content}>
             <div className={classes.toolbar} id="back-to-top-anchor" />
@@ -117,9 +62,50 @@ const Profile = () => {
                             </Typography>
 
                             </CardContent>
-                        <Button variant="outlined" color="primary" textAlign="center" spacing={2}>
-                                   Editar Perfil
-                         </Button>
+                        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                                Editar Perfil
+                              </Button>
+                              <Dialog
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="alert-dialog-title"
+                                aria-describedby="alert-dialog-description"
+                              >
+                                <DialogTitle id="alert-dialog-title">{"Editar Perfil"}</DialogTitle>
+                                <DialogContent>
+                                
+                                        <fromGroup>
+                                            <br/>
+                                            <Input type="text"id="nom" value="Nombre"/>
+                                        </fromGroup>
+                                        <fromGroup>
+                                            <br/>
+                                            <Input type="text"id="ape" value="Apellido"/>
+                                        </fromGroup>
+                                        <fromGroup>
+                                            <br/>
+                                            <Input type="text"id="em" value="E-mail"/>
+                                            
+                                        </fromGroup>
+                                        <fromGroup>
+                                            <br/>
+                                            <Input type="text"id="ci" value="Ciudad"/>
+                                        </fromGroup>
+                                        <fromGroup>
+                                            <br/>
+                                            <textarea id="des" row="5" cols="30">Descripción</textarea>
+                                        </fromGroup>
+                                </DialogContent>
+                                <DialogActions>
+                                  <Button onClick={handleClose} color="primary">
+                                    Cancelar
+                                  </Button>
+                                  <Button onClick={handleClose} color="primary" autoFocus>
+                                    Editar
+                                  </Button>
+                                </DialogActions>
+                              </Dialog>
+    
                          </Card>
                         </Box> 
                 </Grid>
@@ -136,10 +122,6 @@ const Profile = () => {
                     <Chips/>
                     </Box> 
             </Grid>
-
-
-
-
 
                 </Grid>
 
