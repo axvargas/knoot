11eaf00d8b61c00b160b7431382071e6cedaad84
jose598 <morales2010mendoza@hotmail.com>
2020-07-05 23:20:31 -0500

    Modificaciones en Favorito--Tags

--- src/views/favorites/index.js
+++ src/views/favorites/index.js
@@ -11,31 +11,19 @@ const Favorites = () => {
         Cras justo odio, dapibus ac facilisis in, egestas eget quam.
         Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
         Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-        likes: "5", views: "20"},
-        {
-            title: "Titulo 2", description: `Cras mattis consectetur purus sit amet fermentum.
-        Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-        Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-        likes: "5", views: "20"},
+        likes: "5", views: "20",tags: "Texto1 Texto2 Texto3"},
         {
             title: "Titulo 3", description: `Cras mattis consectetur purus sit amet fermentum.
         Cras justo odio, dapibus ac facilisis in, egestas eget quam.
         Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
         Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-        likes: "2", views: "20"},
+        likes: "2", views: "20",tags: "Texto1 Texto2 Texto3"},
         {
             title: "Titulo 4", description: `Cras mattis consectetur purus sit amet fermentum.
         Cras justo odio, dapibus ac facilisis in, egestas eget quam.
         Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
         Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-        likes: "10", views: "20"},
-        {
-            title: "Titulo 5", description: `Cras mattis consectetur purus sit amet fermentum.
-        Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-        Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-        likes: "7", views: "20"},
+        likes: "10", views: "20",tags: "Texto1 Texto2 Texto3"},
     ]
 
     const inSession = true;
@@ -51,7 +39,7 @@ const Favorites = () => {
                                 return (
                                     <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
                                         <MediaCard key={i} title={post.title} description={post.description}
-                                        likes={post.likes} views={post.views} />
+                                        likes={post.likes} views={post.views} tags={post.tags} />
                                     </Grid>
                                 )
                             })
