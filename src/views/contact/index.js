daa9676e3e23eaefddb3add0f8e3588f4619fd71
ltvargas <ltvargas@espol.edu.ec>
2020-07-11 17:22:05 -0500

    estilo contactanos

--- src/views/contact/index.js
+++ src/views/contact/index.js
@@ -3,7 +3,7 @@ import useStyles from '../styles';
 import TextField from '@material-ui/core/TextField';
 import { Typography, Grid,  CssBaseline } from '@material-ui/core';
 import ContactForm from '../../components/contactForm';
-import contact from '../../recursos/contact.jpg';
+import contact from '../../recursos/contact1.png';
 import { makeStyles } from '@material-ui/core/styles';
 
 
@@ -17,12 +17,12 @@ const Contact = () => {
             <main className={classes.content}>
                 <div className={classes.toolbar} id="back-to-top-anchor" />
                 <h1> Contactanos </h1>
-                <Grid container className={classes.container} direction="row" justify="space-between" >
+                <Grid container direction="row" justify="space-between" >
                         <CssBaseline/>
                         <Grid item xs={false} sm={6} md={6} lg={6} style={{textAlign:"center"}} >
-                            <img src={contact} alt="contact" className={classes.imagine} />
+                            <img src={contact} alt="contact"   style={{ maxWidth: '100%'}}  />
                         </Grid>
-                        <Grid items xs={12} sm={6} md={6} lg={6}  >
+                        <Grid items xs={12} sm={6} md={6} lg={6}  style={{flexGrow: 0, paddingLeft: '20px'}}>
                             <ContactForm/>
                         </Grid>
 
