f7b0aae91d158ecf2268abb1e42cde614ec162d1
ltvargas <ltvargas@espol.edu.ec>
2020-08-16 18:11:18 -0500

    actualizacion database

--- Bd_knoot/bd_tablas.sql
+++ Bd_knoot/bd_tablas.sql
@@ -1,12 +1,12 @@
-drop database if exists bd_knoot;
-create database bd_knoot;
-use bd_knoot;
+drop database if exists database_knoot;
+create database database_knoot;
+use database_knoot;
 
 CREATE TABLE IF NOT EXISTS Persona(
 	persona_ID INT  AUTO_INCREMENT PRIMARY KEY,
-	nombre VARCHAR(50),
-	apellido VARCHAR(50),
-    email VARCHAR(30),
+	nombre VARCHAR(50) NOT NULL, 
+	apellido VARCHAR(50) NOT NULL,
+    email VARCHAR(30) NOT NULL,
 	telefono VARCHAR(10),
 	fecha_nacimiento date,
     lugar_origen VARCHAR(50)
@@ -14,24 +14,24 @@ CREATE TABLE IF NOT EXISTS Persona(
 
 CREATE TABLE IF NOT EXISTS Usuario(
 	usuario_ID INT AUTO_INCREMENT PRIMARY KEY,
-	link_foto VARCHAR(30),
-	pass VARCHAR(30),
+	link_foto VARCHAR(300),
+	pass VARCHAR(50) NOT NULL,
     persona_ID INT,
     FOREIGN KEY (persona_ID) REFERENCES Persona(persona_ID)
 );
 
 CREATE TABLE IF NOT EXISTS Habilidad(
 	habilidad_ID INT AUTO_INCREMENT PRIMARY KEY,
-	nombre VARCHAR(50)
+	nombre VARCHAR(50) NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Facultad(
 	facultad_ID INT AUTO_INCREMENT PRIMARY KEY,
-	nombre VARCHAR(50)
+	nombre VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Consumidor(
 	consumidor_ID INT AUTO_INCREMENT PRIMARY KEY,
-	descripcion VARCHAR(150),
+	descripcion VARCHAR(160),
     facultad_ID INT,
     FOREIGN KEY (consumidor_ID) REFERENCES Usuario(usuario_ID),
     FOREIGN KEY (facultad_ID) REFERENCES Facultad(facultad_ID)
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS HabilidadXConsumidor(
 
 CREATE TABLE IF NOT EXISTS Administrador(
 	administrador_ID INT AUTO_INCREMENT PRIMARY KEY,
-	rol VARCHAR(30),
+	rol VARCHAR(30) NOT NULL,
 	FOREIGN KEY (administrador_ID) REFERENCES Usuario(usuario_ID)
 );
 
@@ -57,20 +57,20 @@ CREATE TABLE IF NOT EXISTS Administrador(
 
 CREATE TABLE IF NOT EXISTS Categoria(
 	categoria_ID INT  AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50),
-    descripcion VARCHAR(50)
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Anuncio(
 	anuncio_ID INT AUTO_INCREMENT PRIMARY KEY,
-	nombre VARCHAR(50),
-	descripcion VARCHAR(50),
-    banner VARCHAR(50),
-	estado INT,
-	fecha_inicio DATE,
-    fecha_termino DATE,
-	vacantes VARCHAR(30),
-    cant_interesados VARCHAR(30),
+	nombre VARCHAR(50) NOT NULL,
+	descripcion VARCHAR(50) NOT NULL,
+    banner VARCHAR(300) NOT NULL,
+	estado boolean NOT NULL,
+	fecha_inicio DATE NOT NULL,
+    fecha_termino DATE NOT NULL,
+	vacantes int NOT NULL,
+    cant_interesados int NOT NULL,
     categoria_ID INT,
     consumidor_ID INT,
     FOREIGN KEY (categoria_ID) REFERENCES Categoria(categoria_ID),
@@ -97,15 +97,15 @@ CREATE TABLE IF NOT EXISTS Interesados(
 
 CREATE TABLE IF NOT EXISTS Sugerencia(
 	sugerencia_ID INT  AUTO_INCREMENT PRIMARY KEY,
-    link_detalle VARCHAR(150),
+    link_detalle VARCHAR(160) NOT NULL,
     persona_ID INT,
     FOREIGN KEY (persona_ID) REFERENCES Persona(persona_ID)
 );
 
 CREATE TABLE IF NOT EXISTS Noticia(
 	noticia_ID INT  AUTO_INCREMENT PRIMARY KEY,
-	titulo VARCHAR(50),
-	descripcion VARCHAR(150),
+	titulo VARCHAR(50) NOT NULL,
+	descripcion VARCHAR(150) NOT NULL,
     administrador_ID INT,
     FOREIGN KEY (administrador_ID) REFERENCES Administrador(administrador_ID)
 );
