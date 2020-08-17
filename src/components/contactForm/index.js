48738d81cc56d0434da303a38d9fd64e088bca97
Andres X. Vargas <axvargas@fiec.espol.edu.ec>
2020-08-17 17:08:37 -0500

    Contact Form available to send email

--- src/components/contactForm/index.js
+++ src/components/contactForm/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStyles from './style';
 import TextField from '@material-ui/core/TextField';
-import { Typography,Box,Button } from '@material-ui/core';
+import { Typography, Box, Button } from '@material-ui/core';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
@@ -13,15 +13,35 @@ const ContactForm = () => {
     const classes = useStyles();
 
     const [origen, setOrigen] = React.useState('');
-
+    const [form, setForm] = useState({});
     const handleChange = (event) => {
         setOrigen(event.target.value);
-      };
+    };
 
     //setOrigen(value);
-    
 
-      const provincias = [
+    const handleChangeForm = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSendEmail = async (e) => {
+        console.log("Sending email")
+        const url = "http://localhost:4000/send-email";
+        const response = await fetch(url, {
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(form), // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+
+    const provincias = [
         'Guayas',
         'Manabi',
         'Esmeraldas',
@@ -31,64 +51,75 @@ const ContactForm = () => {
         'Pichincha',
         'Azuay',
         'Otros',
-      ];
+    ];
 
     return (
         <Box className={classes.root} mx="auto">
-            <form className={classes.root} noValidate autoComplete="off" >           
-              <Typography>
-              Nuestra unidad de atención al cliente se encuentra a su disposición para realizar reclamos, consultas y sugerencias. Por favor, llene el siguiente formulario:
+            <form className={classes.root} noValidate autoComplete="off" >
+                <Typography>
+                    Nuestra unidad de atención al cliente se encuentra a su disposición para realizar reclamos, consultas y sugerencias. Por favor, llene el siguiente formulario:
               </Typography>
-                  <br/>
-                  <div className={classes.div}>
-                      <TextField
-                      className={classes.textField}
-                      required
-                      id="nombre"
-                      label="Nombre"
-                      placeholder="Escriba aqui su nombre"
-                      variant="outlined"
-                      />
-                      <TextField
-                      className={classes.textField}
-                      required
-                      id="apellido"
-                      label="Apellido"
-                      placeholder="Escriba aqui su apellido"
-                      variant="outlined"
-                      />
-                  </div>
-                  <TextField
-                      required
-                      id="email"
-                      className={classes.textField}
-                      label="Correo electrónico de contacto"
-                      placeholder="Escriba aqui su correo electrónico"
-                      variant="outlined"
-                  />
+                <br />
+                <div className={classes.div}>
+                    <TextField
+                        className={classes.textField}
+                        required
+                        name="nombre"
+                        id="nombre"
+                        label="Nombre"
+                        placeholder="Escriba aqui su nombre"
+                        variant="outlined"
+                        onChange={handleChangeForm}
 
-                  <TextField
-                      required
-                      id="telefono"
-                      className={classes.textField}
-                      label="Telefono"
-                      placeholder="Escriba aqui su telefono"
-                      variant="outlined"
-                  />
+                    />
+                    <TextField
+                        className={classes.textField}
+                        required
+                        id="apellido"
+                        name="apellido"
+                        label="Apellido"
+                        placeholder="Escriba aqui su apellido"
+                        variant="outlined"
+                        onChange={handleChangeForm}
+                    />
+                </div>
+                <TextField
+                    required
+                    id="email"
+                    name="email"
+                    className={classes.textField}
+                    label="Correo electrónico de contacto"
+                    placeholder="Escriba aqui su correo electrónico"
+                    variant="outlined"
+                    onChange={handleChangeForm}
+                />
+
+                <TextField
+                    required
+                    id="telefono"
+                    name="telefono"
+                    className={classes.textField}
+                    label="Telefono"
+                    placeholder="Escriba aqui su telefono"
+                    variant="outlined"
+                    onChange={handleChangeForm}
+                />
 
                 <TextField
                     required
                     type="date"
+                    name="fechaNacimiento"
                     id="fechaNacimiento"
                     label="Fecha de nacimiento"
                     placeholder="Escriba aqui su fecha de nacimiento"
                     variant="outlined"
                     className={classes.textField}
                     InputLabelProps={{
-                    shrink: true,
+                        shrink: true,
                     }}
+                    onChange={handleChangeForm}
                 />
-                   <FormControl variant="outlined" className={classes.formControl}>
+                <FormControl variant="outlined" className={classes.formControl}>
                     <InputLabel id="demo-simple-select-outlined-label">Lugar de origen</InputLabel>
                     <Select
                         labelId="demo-simple-select-outlined-label"
@@ -99,35 +130,38 @@ const ContactForm = () => {
                     >
 
                         {provincias.map((origen) => (
-                                    <MenuItem key={origen} value={origen}>
-                                    {origen}
-                                    </MenuItem>
-                                ))}
+                            <MenuItem key={origen} value={origen}>
+                                {origen}
+                            </MenuItem>
+                        ))}
                     </Select>
                 </FormControl>
 
-                  <TextField
-                      id="outlined-multiline-static"
-                      className={classes.textField}
-                      label="Detalle"
-                      multiline
-                      rows={4}
-                      placeholder="Escriba su mensaje aqui"
-                      variant="outlined"
-                      />
-                    <Button
+                <TextField
+                    id="outlined-multiline-static"
+                    className={classes.textField}
+                    label="Detalle"
+                    name="detalle"
+                    multiline
+                    rows={4}
+                    placeholder="Escriba su mensaje aqui"
+                    variant="outlined"
+                    onChange={handleChangeForm}
+                />
+                <Button
                     variant="contained"
                     //color="primary"
                     size="large"
                     className={classes.button}
-                    endIcon={<SendIcon/>}
-                    >
-                     Enviar 
+                    endIcon={<SendIcon />}
+                    onClick={handleSendEmail}
+                >
+                    Enviar
                     </Button>
-                            
+
             </form>
         </Box>
-        
+
     );
 }
 export default ContactForm;
