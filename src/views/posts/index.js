1c0dc875eb5aac38977010371c4eff82015f074e
ltvargas <ltvargas@espol.edu.ec>
2020-09-17 14:16:48 -0500

    retiro

--- src/views/posts/index.js
+++ src/views/posts/index.js
@@ -40,7 +40,7 @@ const Posts = () => {
                  var habilidades="";
                  post.habilidad.map(item => habilidades+=item.nombre+" " )
                  habilidades = habilidades.substring(0, habilidades.length - 1);
-                 if(post.usuario=="12"){
+                
                  return (
                      
                      <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
@@ -50,7 +50,7 @@ const Posts = () => {
                          />
                      </Grid>
                    
-                 ) }
+                 ) 
              })
              
          }
